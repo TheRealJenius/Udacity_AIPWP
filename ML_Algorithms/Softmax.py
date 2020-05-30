403a57c4c98bad34e55ea82cbd64ad1e9a24927b
Jesse Appiah <TheRealJenius@gmail.com>
2020-05-30 17:16:01 +0100

    Added their solution for reference

--- ML_Algorithms/Softmax.py
+++ ML_Algorithms/Softmax.py
@@ -9,3 +9,21 @@ def softmax(L):
     for i in range(len(L)): # Iterating through the lenght of the array of numbers
         sm_function_result.append(np.exp(L[i])/total_exp) # remember we can't directly assign to an empty list, we need to append the result at the end
     return sm_function_result # return the result so it can be used in another function
+
+''' Their solution
+
+import numpy as np
+
+def softmax(L):
+    expL = np.exp(L)
+    sumExpL = sum(expL)
+    result = []
+    for i in expL:
+        result.append(i*1.0/sumExpL)
+    return result
+    
+    # Note: The function np.divide can also be used here, as follows:
+    # def softmax(L):
+    #     expL = np.exp(L)
+    #     return np.divide (expL, expL.sum())
+'''
